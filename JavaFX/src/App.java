2ff49859a7217a78b21653744b1918ceb9b49abe
MarcosLenilson <janajesuslelemarcos@gmail.com>
2023-09-03 04:37:46 -0300

    JavaFX primeiro commit

---
+++ JavaFX/src/App.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Academia Dev");
+        Button botao = new Button("Cliqui aqui");
+        botao.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Clicou no botão ");
+            }
+        });
+        StackPane root = new StackPane();
+        root.getChildren().addAll(botao);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
